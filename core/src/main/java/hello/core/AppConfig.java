8aabb785db23d0a2752de6aa7befb92c7a84b92c
semin-1432 <2007ksm@naver.com>
2022-01-24 21:31:54 +0900

    Member management example not use spring

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,7 +2,10 @@ package hello.core;
 
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +13,22 @@ import hello.core.member.MemoryMemberRepository;
 public class AppConfig {
     //MemberServiceImpl을 만들고, 내가 만든 MemberServiceImpl은 MemoryMemberRepository를 사용할거야
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+
+        return new MemberServiceImpl(memberRepository());
     }
 
     //MemoryMemberRepository와 FixDiscountPolicy를 넣고 계산한 객체를 반환한다.
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(
+            memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
