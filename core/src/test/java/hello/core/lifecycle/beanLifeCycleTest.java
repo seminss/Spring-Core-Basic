bea60cc96c21ac2272f30d04d9721c1fa88568a1
semin-1432 <2007ksm@naver.com>
2022-02-08 11:26:27 +0900

    @PostConstruct, @PreDestroy

--- core/src/test/java/hello/core/lifecycle/beanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/beanLifeCycleTest.java
@@ -16,7 +16,7 @@ public class beanLifeCycleTest {
     }
     @Configuration
     static class LifeCycleConfig{
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
